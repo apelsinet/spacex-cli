e90121d2b03a51207c608bb6c9e1f69ce92220b1
apelsinet <mattiasprada@gmail.com>
2018-02-18 23:31:07 +0100

    Port to blessed

--- index.js
+++ index.js
@@ -1,25 +1,135 @@
-const vorpal = require('vorpal')();
+const blessed = require('blessed');
+const moment = require('moment');
 require('es6-promise').polyfill();
 require('isomorphic-fetch');
-const moment = require('moment');
-const window = require('window-size');
+const pjson = require('./package.json');
+const screen = blessed.screen({
+	smartCSR: true,
+});
+const APP_TITLE = `SpaceX CLI ${pjson.version}`;
 const API_URL = 'https://api.spacexdata.com/v2/launches/upcoming';
 const STATUS = {
 	FETCHING: 'FETCHING',
 	SUCCESS: 'SUCCESS',
 	ERROR: 'ERROR',
 };
+screen.title = APP_TITLE;
 
-class RenderUi {
+class App {
 	constructor() {
 		this.fetchStatus = null;
 		this.data = null;
-		this.interval = null;
+		this.background = blessed.box({
+			top: 'center',
+			left: 'center',
+			width: '100%',
+			height: '100%',
+			tags: true,
+			style: {
+				fg: 'white',
+				bg: 'blue',
+				hover: {
+					bg: 'green',
+				},
+			},
+		});
+
+		this.container = blessed.box({
+			parent: this.background,
+			label: APP_TITLE,
+			top: 'center',
+			left: 'center',
+			width: '90%',
+			height: '90%',
+			border: {
+				type: 'line',
+			},
+			style: {
+				bg: 'grey',
+				fg: 'black',
+				border: {
+					bg: 'grey',
+					fg: 'white',
+				},
+				label: {
+					bg: 'grey',
+					fg: 'white',
+				},
+			},
+			shadow: true,
+		});
+
+		this.loadingIndicator = blessed.box({
+			tags: true,
+			parent: this.container,
+			content: '\n{center}Fetching...{/center}',
+			top: 'center',
+			left: 'center',
+			height: 3,
+			style: {
+				bg: 'red',
+				fg: 'white',
+			},
+			hidden: true,
+		});
+
+		this.dataContainer = blessed.box({
+			tags: true,
+			parent: this.container,
+			top: 'center',
+			left: 'center',
+			width: '90%',
+			height: '90%',
+			padding: 1,
+			border: {
+				type: 'line',
+			},
+			style: {
+				bg: 'black',
+				fg: 'green',
+				border: {
+					bg: 'grey',
+					fg: 'white',
+				},
+			},
+			hidden: true,
+		});
+
+		this.content = blessed.box({
+			parent: this.dataContainer,
+			style: {
+				bg: 'black',
+				fg: 'green',
+			},
+		});
+
+		screen.append(this.background);
+		screen.key(['escape', 'q', 'C-c'], function(ch, key) {
+			return process.exit(0);
+		});
+		this.background.focus();
+		screen.render();
+	}
+
+	renderContent() {
+		this.loadingIndicator.hide();
+		this.dataContainer.hide();
+		switch (this.fetchStatus) {
+			case STATUS.FETCHING:
+				this.loadingIndicator.show();
+				break;
+			case STATUS.SUCCESS:
+				this.updateContent();
+				this.dataContainer.show();
+				break;
+		}
+		screen.render();
 	}
 
 	getLaunchData() {
 		if (!this.fetchStatus) {
 			this.fetchStatus = STATUS.FETCHING;
+			this.renderContent();
 			fetch('https://api.spacexdata.com/v2/launches/upcoming')
 				.then(response => {
 					if (response.status >= 400) {
@@ -30,6 +140,7 @@ class RenderUi {
 				.then(data => {
 					this.data = data;
 					this.fetchStatus = STATUS.SUCCESS;
+					this.renderContent();
 				})
 				.catch(err => {
 					this.fetchStatus = STATUS.ERROR;
@@ -46,70 +157,27 @@ class RenderUi {
 		return Math.floor(Math.random() * Math.floor(max));
 	}
 
-	draw() {
-		this.getLaunchData();
-		this.interval = setInterval(() => {
-			vorpal.ui.redraw(this.ui());
-		}, 100);
+	updateContent() {
+		this.content.setContent(
+			this.getNextLaunch() +
+				this.getLaunchSite() +
+				this.getRocketData() +
+				this.getPayloadData(),
+		);
 	}
 
-	renderLineTop() {
-		return `╔${'═'.repeat(window.width - 2)}╗\n`;
+	getNextLaunch() {
+		return `Next launch ${this.getLaunchDateFromNow(this.data)}.\n`;
 	}
 
-	renderLineMiddle() {
-		return `╠${'═'.repeat(window.width - 2)}╣\n`;
-	}
-
-	renderLineBottom() {
-		return `╚${'═'.repeat(window.width - 2)}╝\n`;
-	}
-
-	renderRow(message) {
-		return `║${message}${' '.repeat(window.width - 2 - message.length)}║\n`;
-	}
-
-	renderHeader() {
-		let message = '';
-		switch (this.fetchStatus) {
-			case STATUS.FETCHING:
-				message = `Fetching${'.'.repeat(this.getRandomInt(5))}`;
-				break;
-			case STATUS.SUCCESS:
-				message = `Next launch ${this.getLaunchDateFromNow(
-					this.data,
-				)}.`;
-				break;
-			case STATUS.ERROR:
-				message = 'Could not connect to SpaceX API';
-				break;
-		}
-
-		return this.renderLineTop() + this.renderRow(message);
-	}
-
-	renderLaunchSite() {
-		if (!this.data) {
-			return this.renderRow('');
-		}
+	getLaunchSite() {
 		const launchSite = this.data[0].launch_site.site_name_long;
-		const message = `Launch site: ${launchSite}`;
-		return this.renderRow(message);
+		return `Launch site: ${launchSite}\n`;
 	}
 
-	renderAnimation() {
-		const timeFactor = parseInt(moment().format('s'), 10) / 60;
-		const smokeLength = (window.width - 6) * timeFactor;
-		const message = `${'~'.repeat(smokeLength)}▶▭▭▸`;
-		return this.renderRow(message);
-	}
-
-	renderRocketData() {
-		if (!this.data) {
-			return this.renderRow('');
-		}
+	getRocketData() {
 		const rocketType = this.data[0].rocket.rocket_name;
-		const messageUpper = `Rocket: ${rocketType}`;
+		const messageUpper = `Rocket: ${rocketType}\n`;
 		let messageLower = '';
 		const { cores } = this.data[0].rocket.first_stage;
 		if (cores.length === 1) {
@@ -117,45 +185,28 @@ class RenderUi {
 				cores[0].reused
 					? ', reused (Flight ' + cores[0].flight + ')'
 					: ''
-			}`;
+			}\n`;
 		} else {
 			messageLower = `3 cores, S/N: ${cores[0].core_serial} ${
 				cores[1].core_serial
-			} ${cores[2].core_serial}`;
+			} ${cores[2].core_serial}\n`;
 		}
 
-		return this.renderRow(messageUpper) + this.renderRow(messageLower);
+		return messageUpper + messageLower;
 	}
 
-	renderPayloadData() {
-		if (!this.data) {
-			return this.renderRow('');
-		}
+	getPayloadData() {
 		const payload = this.data[0].rocket.second_stage.payloads[0];
-		return (
-			this.renderRow(
-				`Payload: ${payload.payload_type}, ID: ${
-					payload.payload_id
-				}, customer: ${payload.customers[0]}`,
-			) +
-			this.renderRow(
-				`Orbit: ${payload.orbit}, mass: ${payload.payload_mass_kg}kg`,
-			)
-		);
-	}
+		const messageUpper = `Payload: ${payload.payload_type}, ID: ${
+			payload.payload_id
+		}, customer: ${payload.customers[0]}\n`;
+		const messageLower = `Orbit: ${payload.orbit}, mass: ${
+			payload.payload_mass_kg
+		}kg\n`;
 
-	ui() {
-		return (
-			this.renderHeader() +
-			this.renderLaunchSite() +
-			this.renderAnimation() +
-			this.renderRocketData() +
-			this.renderLineMiddle() +
-			this.renderPayloadData() +
-			this.renderLineBottom()
-		);
+		return messageUpper + messageLower;
 	}
 }
 
-const space = new RenderUi();
-space.draw();
+const app = new App();
+app.getLaunchData();
